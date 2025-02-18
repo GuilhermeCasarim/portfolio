7af5b1d863fa8f849fafe53ebfd07b89bc2238fd
GuilhermeCasarim <140670537+GuilhermeCasarim@users.noreply.github.com>
2025-02-18 12:33:56 -0300

    ajustando texto do projetos

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -3,10 +3,7 @@ const ProjectsContainer = () => {
     <section className="projects-container">
       <h2>Projetos</h2>
       <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore at
-        rem fugit provident dolorum exercitationem, ducimus quisquam ratione
-        cumque reiciendis accusamus totam, veniam iusto numquam distinctio.
-        Dignissimos labore aspernatur pariatur?
+        Confira a seguir os projetos que foram/estão sendo feitos por mim. Alguns já estão prontos, e outros ainda estão sendo realizados. Espero que goste!
       </p>
       <a href="https://github.com/GuilhermeCasarim" className="btn" target="_blank">
         Ver Projetos
